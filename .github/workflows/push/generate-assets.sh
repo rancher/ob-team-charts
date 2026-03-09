54937493b41de06107a46caf220d238a6a3a696d
Siva Kanakala <siva.kanakala@suse.com>
2026-03-09 18:09:13 +0530

    limit-asset-generation

--- .github/workflows/push/generate-assets.sh
+++ .github/workflows/push/generate-assets.sh
@@ -17,10 +17,15 @@ ensure_branch_file
 
 ASSET_LABEL="${ASSET_LABEL:-Initial}"
 CHART_NAMES=$(cut -d',' -f1 "$BRANCH_FILE" | cut -d'/' -f1 | sort -u | tr '\n' ',' | sed 's/,$//' | sed 's/,/, /g')
+BASE_REF=${BASE_REF:-upstream/main}
+
+MODIFIED_PACKAGES=$(git -C "$CHARTS_DIR" diff --name-only "$BASE_REF"...HEAD | grep '^packages/' | cut -d/ -f2-3 | sort -u || true)
 
 while IFS=, read -r chart_full_version PACKAGE_PATH; do
-  make -C "$CHARTS_DIR" charts PACKAGE="$PACKAGE_PATH" USE_CACHE=true
-  summary "  - Generated assets for \`$PACKAGE_PATH\`"
+  if echo "$MODIFIED_PACKAGES" | grep -q "^$PACKAGE_PATH$"; then
+     make -C "$CHARTS_DIR" charts PACKAGE="$PACKAGE_PATH" USE_CACHE=true
+     summary "  - Generated assets for \`$PACKAGE_PATH\`"
+  fi
 done < "$BRANCH_FILE"
 
 commit_if_changed "chore(charts): $ASSET_LABEL \`${CHART_NAMES}\` chart assets build"
